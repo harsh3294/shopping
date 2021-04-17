c69213c58530fd9fd721d178f895dbd51fd783f3
harsh3294 <harshc3294@gmail.com>
2021-04-17 23:39:38 +0530

    deployment to firebase

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8001",
+  // baseURL: "http://localhost:8001",
+  baseURL: "https://shoppers-backend.herokuapp.com/",
 });
 export default instance;
