fc60bc75c649b66b968199770d37f6ae4118dc85
harsh3294 <harshc3294@gmail.com>
2021-04-17 22:54:59 +0530

    deployment v9

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-export const config = {
+export const config1 = {
   serviceId: "VA7d8e26e530c754b86d35a0eb555124ae",
   accountId: "ACf5ce70582fdf00286254ade61c6a0856",
   authToken: "5302c11a44f1f20965d84b4a47495348",
