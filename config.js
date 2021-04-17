0d2368272f5e74f9df8a0b12eac29f52a2d8631a
harsh3294 <harshc3294@gmail.com>
2021-04-17 22:50:10 +0530

    deployment v8

---
+++ config.js
@@ -0,0 +1,5 @@
+export const config = {
+  serviceId: "VA7d8e26e530c754b86d35a0eb555124ae",
+  accountId: "ACf5ce70582fdf00286254ade61c6a0856",
+  authToken: "5302c11a44f1f20965d84b4a47495348",
+};
---
+++ stripekey.js
@@ -0,0 +1,2 @@
+export const key =
+  "sk_test_51IXgpiSBGmQwpVF99weLXDixoTuZdf74xG3UPDCVVajpVtOtg4nNw83FLTrF6xXZgP8RnD5CreN3UpVwQSEkFWjq00nZj8rFrW";
